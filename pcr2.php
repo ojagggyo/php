fc8aefc2111950f478f35056519b3bb85bf0c457
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2022-12-05 21:23:08 +0900

    Create pcr2.php

---
+++ pcr2.php
@@ -0,0 +1,13 @@
+<?php
+if($fp = fopen('https://toyokeizai.net/sp/visual/tko/covid19/csv/death_total.csv','r')){
+  echo "pcr1 ({csv:[";
+  while ($line = fgets($fp)){
+    $arrayList = preg_split("[,]",$line);
+    $arrayList[1] = trim($arrayList[1]);
+    echo "[\"$arrayList[0]\",\"$arrayList[1]\"],";
+  }
+  echo "]})";
+  fclose($fp);
+}else{
+}
+?>
